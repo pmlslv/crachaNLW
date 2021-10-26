6bae500ec413b4869d92394ec693ac4f2b20d6a7
Pamela Silva <90668027+pmlslv@users.noreply.github.com>
2021-10-26 15:40:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const socialMedia = {
+  github: "pmlslv",
+  youtube: "",
+  facebook: "",
+  instagram: "pmlslv",
+  twitter: ""
+};
+
+function changeSocialMedia() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+
+    li.children[0].href = `https://${social}.com/${socialMedia[social]}`;
+  }
+}
+
+changeSocialMedia();
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${socialMedia.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userGitHub.href = data.html_url;
+      userAvatar.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+
+getGitHubProfileInfos();
